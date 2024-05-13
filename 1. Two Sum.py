c0f5eea22b82100cd7cb91d78b97a48c5189c680
Aftab <aftabrafi9@gmail.com>
2024-05-13 21:10:46 +0600

    first commit

---
+++ 1. Two Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum (self, nums, target):
+        prevMap = {}                        # value : index
+
+        for i, n in enumerate(nums):        
+            diff = target - n
+            if diff in prevMap:             # if first value exists
+                return [prevMap[diff], i]   # return solution
+            prevMap[n] = i                  # otherwise add n to map
+        return    
